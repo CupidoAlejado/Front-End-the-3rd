580200638dd323edff2fb43c61809525714a37b9
Luis Fernano <149402596+CupidoAlejado@users.noreply.github.com>
2024-08-31 02:23:23 +0000

    receba

--- Front-End-the-3rd-main/AprendeMais/main.js
+++ Front-End-the-3rd-main/AprendeMais/main.js
@@ -1,20 +1,24 @@
 function acertos() {
-    alert('deu certo')
+    res = 0
+    r = 0
+    cont = 0
     if (cont>0) {
         return false
     }
-    var opcao = document.getElementsByName('r');
-    var opcaon;
-    for (var cont = 0; cont < opcao.length; cont++) {
-        if (opcao[cont].checked) {
-            opcaon = opcao[cont].value;
-            if (opcaon == "true") {
-                r++
+    else{
+    var opcao = document.getElementsByName('re');
+    var opcao_value = 0;
+    for (var conter = 0; conter < opcao.length; conter++) {
+        if (opcao[conter].checked) {
+            opcao_value = opcao[conter].value;
+            if (opcao_value == "true") {
+                res++
             }
         }
     }
-    var resultado = document.getElementById("resultado")
+
     var nome = document.getElementById("nome").value
-    resultado.innerHTML = "Parabéns " + nome + "! Você acertou " + r + " questões"
+    document.getElementById("resultado").innerHTML = "Parabéns " + nome + "! Você acertou " + res + " questões"
     cont++
+    }
 }
